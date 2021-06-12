d48429533b5ab06ca46ff7d1aef825807a06ecd6
Rick Hallett <rickhallett@live.co.uk>
2021-06-12 13:07:02 +0100

    latest shib positions by interval

--- index.js
+++ index.js
@@ -4,15 +4,12 @@ import dotenv from 'dotenv';
 import chalk from 'chalk';
 import { create, all } from 'mathjs';
 import winston from 'winston';
-
-// const winston = require('winston');
+import { EventEmitter } from 'events';
 
 dotenv.config();
 
 const blue = chalk.blue;
 const orange = chalk.keyword('orange');
-
-// logger.info({ env: process.env });
 
 const { format } = winston;
 const { combine, timestamp, label, printf } = format;
@@ -53,6 +50,8 @@ const bProxy = new Binance().options({
 const mathx = create(all);
 mathx.config({ number: 'BigNumber', precision: 64 });
 
+const eventBus = new EventEmitter();
+
 const cacheTickers = async () => {
   let ticker;
 
@@ -60,7 +59,8 @@ const cacheTickers = async () => {
     ticker = JSON.parse(fs.readFileSync('./cache/ticker.json').toString());
     logger.info('Retrieved tickers from cache');
   } catch (error) {
-    logger.error(error.message);
+    handleError(error);
+    logger.error(error);
     if (error.message.substring(0, 6) === 'ENOENT') {
       logger.info('No cache. Retrieving from API.');
     }
@@ -71,11 +71,13 @@ const cacheTickers = async () => {
       ticker = await bProxy.prices();
       logger.info('Retrieved tickers from API');
     } catch (error) {
+      handleError(error);
       logger.error(error);
     }
   }
 
   fs.writeFileSync('./cache/ticker.json', JSON.stringify(ticker));
+  return ticker;
 };
 
 const retrieveShibCache = () => {
@@ -98,21 +100,23 @@ const getLatestSymbolRate = async (symbol) => {
     ticker = JSON.parse(
       fs.readFileSync(`./cache/latest-${symbol}.json`).toString()
     );
-    logger.log(`\nRetrieved ${symbol} from cache`);
+    logger.info(`\nRetrieved ${symbol} from cache`);
   } catch (error) {
-    logger.error(error.message);
+    handleError(error);
+    logger.error(error);
     if (error.message.substring(0, 6) === 'ENOENT') {
-      logger.log('No cache. Retrieving from API.');
+      logger.info('No cache. Retrieving from API.');
     }
   }
 
   if (!ticker) {
     try {
-      logger.log(`\nRetrieving latest price for ${symbol}...`);
+      logger.info(`\nRetrieving latest price for ${symbol}...`);
       ticker = await bProxy.prices(symbol);
       logger.info(`Price of ${symbol}:`, ticker[symbol]);
       fs.writeFileSync(`./cache/latest-${symbol}.json`, JSON.stringify(ticker));
     } catch (error) {
+      handleError(error);
       logger.error(error);
     }
   }
@@ -129,31 +133,38 @@ const getCurrentBalances = async () => {
   try {
     logger.info('Retrieving latest balances from api...');
     balances = await bProxy.balance();
-    // console.info('balances()', balances);
     fs.writeFileSync('./cache/balances.json', JSON.stringify(balances));
+    fs.writeFileSync(
+      './cache/shib-balances.json',
+      JSON.stringify(balances.SHIB)
+    );
     logger.info(`SHIB balance: ${balances.SHIB.available}`);
+    return balances;
   } catch (error) {
+    handleError(error);
     logger.error(error);
   }
 };
 
 class Scheduler {
-  constructor() {
-    this.positions = {};
+  constructor(symbol) {
+    this.symbol = symbol;
+    this.positions = [];
   }
 
   addPostion(position) {
-    this.positions[position.symbol] = {
+    this.positions.push({
       price: position.price,
       date: position.date,
       rDate: position.readableDate,
-    };
+    });
   }
 }
 class Position {
-  constructor({ symbol, price }) {
+  constructor({ symbol, price, qty }) {
     this.symbol = symbol;
-    this.price = mathx.bignumber(price || 0);
+    this.price = price || 0;
+    this.qty = qty || 0;
     this.date = Date.now();
     this.readableDate = Date(this.date);
   }
@@ -162,21 +173,74 @@ class Position {
     return mathx.divide(mathx.bignumber(curprice), this.price).multiply(100);
   }
 
-  buy() {}
+  buy(qty) {
+    bProxy.marketBuy(this.symbol, qty);
+  }
 
-  sell() {}
+  sell(qty) {
+    bProxy.marketSell(this.symbol, qty);
+  }
 }
 
-const main = async () => {
-  const positions = new Scheduler();
+const cachers = async () => {
   await cacheTickers();
   const shibCache = retrieveShibCache();
-  // logger.log({ shibCache });
-  const shibLatest = new Position(await getLatestSymbolRate('SHIBUSDT'));
-  // logger.log({ shibLatest });
-  positions.addPostion(shibLatest);
-  // logger.log({ positions: JSON.stringify(positions) });
-  await getCurrentBalances();
+};
+
+const getBalances = async () => {
+  const balances = await getCurrentBalances();
+  logger.info(`GBP balances: ${balances.GBP}`);
+  logger.info(`USDT balances: ${balances.USDT}`);
+  logger.info(`SHIB balances: ${balances.SHIB}`);
+  return balances;
+};
+
+const tickerInterval = async (symbol, positions) => {
+  const stopInterval = (i, cnxToken) => {
+    if (i > 3) {
+      clearInterval(cnxToken);
+      eventBus.emit('INTERVAL_COMPLETE', positions);
+    }
+  };
+
+  let i = 0;
+  const cnxToken = setInterval(async () => {
+    logger.info('Getting latest SHIB...');
+    const posLatest = new Position(await getLatestSymbolRate('SHIBUSDT'));
+    positions.addPostion(posLatest);
+    logger.info(`added latest position: ${posLatest.symbol}`);
+    stopInterval(i++, cnxToken, positions);
+  }, 60000);
+};
+
+const main = async () => {
+  const scheduler = new Scheduler();
+
+  tickerInterval('SHIBUSDT', scheduler);
+  // logger.info({ scheduler: JSON.stringify(scheduler) });
+};
+
+eventBus.on('INTERVAL_COMPLETE', (scheduler) => {
+  fs.writeFileSync(
+    './cache/shib-positions.json',
+    JSON.stringify(scheduler.positions)
+  );
+  logger.info(`Added ${scheduler.positions} to scheduler`);
+});
+
+const handleError = (error) => {
+  console.error(error);
 };
 
 main();
+
+/**
+ * Pseudo
+ *
+ * get new pos
+ * check perc diff from last pos
+ *
+ * If  number  rises  10%  from  last low BUY
+ * If  number  falls  7.5%  from  last  high  SELL
+ * If number falls 5% below buy value SELL
+ */
